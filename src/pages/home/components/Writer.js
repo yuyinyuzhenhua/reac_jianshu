4835538cd8594c24adb7bb55a18d0d479f6f258e
yuzhenhua <yuzhenhua@bjhike.com>
2020-06-22 13:03:12 +0800

    a

--- src/pages/home/components/Writer.js
+++ src/pages/home/components/Writer.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
+import { WriterWrapper } from '../style'
 
 class Writer extends Component{
     render(){
-        return <div>Writer</div>
+        return <WriterWrapper>Writer</WriterWrapper>
     }
 }
 
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -87,6 +87,11 @@ export const RecommendWrapper = styled.div`
     }
 `
 
-export const RecommendItem = styled.img`
-    
+export const WriterWrapper = styled.div`
+    width: 278px;
+    border: 1px solid #dcdcdc;
+    border-radius: 3px;
+    height: 300px;
+    line-height: 300px;
+    text-align: center;
 `
